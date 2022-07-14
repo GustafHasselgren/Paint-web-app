55727df4a2db6e7b8f93afb415de6753ec420bb0
Gustaf Ericson <gustaf.ericson2@gmail.com>
2022-07-14 18:21:41 +0200

    Rensning, Flash msg, färdigställa manage.

--- Paint web app/main.py
+++ Paint web app/main.py
@@ -4,3 +4,19 @@ app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True)
+
+# TODO
+#   Nu:
+#   Hantera Paints & Methods                                X
+#   Feedback - Message flash                                X
+#   Fixa Mobile media
+#   
+#   Snart:
+#   
+#   
+#   Utöka Paints - Typ, Tillverkare, Färg-grupp - Sortera på Färg>Typ
+#   Lös 'Hover' på kort
+#   
+#   Kul längre fram:
+#   Stats - Mest använda färg, vanligaste steget, etc.
+#
